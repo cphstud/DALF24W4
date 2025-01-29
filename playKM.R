1ef15e136d859f267cee5731466f4e5f5a207b04
thor <thor@macbookusiness2.home>
2025-01-29 13:26:02 +0100

    first run for computing distance

--- playKM.R
+++ playKM.R
@@ -12,17 +12,33 @@ plot(df)
 # (b) Assign each observation to the cluster whose centroid is closest
 
 #1. Randomly assign a number, from 1 to K, to each of the observations.
+k=3
 df$cluster=round(runif(20,min=1,max = 3),0)
 
 ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()
 
 #2. Find centroids
 # find means of the observation in each cluster
-centroids=df %>% group_by(cluster) %>% summarise(mx=mean(X),
-                                                 my=mean(Y))
+centroids=df %>% group_by(cluster) %>% summarise(X=mean(X),
+                                                 Y=mean(Y))
 ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()+
-  geom_point(data=centroids, aes(x=mx,y=my, size=4,shape = as.factor(cluster)))
+  geom_point(data=centroids, aes(x=X,y=Y, size=4,shape = as.factor(cluster)))
 
+#assign each obs - uanset cluster - to the closest centercluster
+# closest = min(√[(x2 – x1)2 + (y2 – y1)2])
+# sqrt((cx-obsx)^2 + (cy-obsy)^2)
+
+for (i in (1:nrow(df))) {
+  i=1
+  # vector to collect the distances for the point to the respective cluster
+  colv=vector()
+  for (j in (1:k)) {
+    mdist=sqrt(((centroids[[j,2]]-df[[i,1]])^2)+((centroids[[j,3]]-df[[i,2]])^2))
+    colv[j]=mdist
+  }
+  newcluster=which.min(colv)
+  df[i,'cluster']=newcluster
+}
 
 
 
