302193d29605722361a3df8d8e2a4ab3d2dea52a
thor <thor@cphbusiness.dk>
2025-01-29 15:59:31 +0100

    vectorized version of inner loop

--- playKM.R
+++ playKM.R
@@ -13,85 +13,51 @@ plot(df)
 
 #1. Randomly assign a number, from 1 to K, to each of the observations.
 k=3
-df$cluster=round(runif(20,min=1,max = 3),0)
+df$cluster=round(runif(20,min=1,max = k),0)
 
-ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()
-
-#2. Find centroids
-# find means of the observation in each cluster
-centroids=df %>% group_by(cluster) %>% summarise(X=mean(X),
-                                                 Y=mean(Y))
-ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()+
-  geom_point(data=centroids, aes(x=X,y=Y, size=4,shape = as.factor(cluster)))
-
-#assign each obs - uanset cluster - to the closest centercluster
-# closest = min(√[(x2 – x1)2 + (y2 – y1)2])
-# sqrt((cx-obsx)^2 + (cy-obsy)^2)
-
-for (i in (1:nrow(df))) {
-  # vector to collect the distances for the point to the respective cluster
-  colv=vector()
-  for (j in (1:k)) {
-    mdist=sqrt(((centroids[[j,2]]-df[[i,1]])^2)+((centroids[[j,3]]-df[[i,2]])^2))
-    colv[j]=mdist
+#Loop until changecounter stops
+isChanged=1
+roundcounter=0
+while (isChanged == 1) {
+  print("GO")
+  changecounter=0
+  roundcounter=roundcounter+1
+  
+  #2. Find centroids - find means of the observation in each cluster
+  centroids=df %>% group_by(cluster) %>% summarise(X=mean(X),
+                                                 Y=mean(Y)) %>% ungroup()
+  
+  # plot the centroids and cluster
+  p <- ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()+
+    geom_point(data=centroids, aes(x=X,y=Y, size=4,shape = as.factor(cluster)))
+  print(p)
+  Sys.sleep(2)
+  
+  
+  # assign each obs - uanset cluster - to the closest centercluster
+  # closest = min(√[(x2 – x1)2 + (y2 – y1)2]) or R: sqrt((cx-obsx)^2 + (cy-obsy)^2)
+  for (i in (1:nrow(df))) {
+    # vector to collect the distances for the point to the respective cluster
+    mdist=sapply(1:k,function(j) {
+      sqrt((centroids$X[j]-df$X[i])^2 + 
+             (centroids$Y[j]-df$Y[i])^2+ 
+             (centroids$Z[j]-df$Z[i])^2)
+    })
+    newcluster=which.min(mdist)
+    if(df[i,'cluster']!=newcluster) {
+      df[i,'cluster']=newcluster
+      changecounter=changecounter+1
+    }
   }
-  newcluster=which.min(colv)
-  df[i,'cluster']=newcluster
+  
+  #plot the new clusterassignment
+  p <- ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()+
+    geom_point(data=centroids, aes(x=X,y=Y, size=4,shape = as.factor(cluster)))+
+    ggtitle(paste("Got this",roundcounter))
+  print(p)
+  Sys.sleep(5)
+  
+  # Should we continue?
+  isChanged = ifelse(changecounter > 0,1,0)
+  print(paste("SLEEP",isChanged))
 }
-ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()+
-  geom_point(data=centroids, aes(x=X,y=Y, size=4,shape = as.factor(cluster)))
-
-#SECOND RUN
-# recompute the centroids
-centroids=df %>% group_by(cluster) %>% summarise(X=mean(X),
-                                                 Y=mean(Y))
-
-ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()+
-  geom_point(data=centroids, aes(x=X,y=Y, size=4,shape = as.factor(cluster)))
-
-
-#THIRD RUN
-#re-assign the observations based on distance
-for (i in (1:nrow(df))) {
-  # vector to collect the distances for the point to the respective cluster
-  colv=vector()
-  for (j in (1:k)) {
-    mdist=sqrt(((centroids[[j,2]]-df[[i,1]])^2)+((centroids[[j,3]]-df[[i,2]])^2))
-    colv[j]=mdist
-  }
-  newcluster=which.min(colv)
-  df[i,'cluster']=newcluster
-}
-
-centroids=df %>% group_by(cluster) %>% summarise(X=mean(X),
-                                                 Y=mean(Y))
-
-ggplot(df, aes(x=X,y=Y, colour = as.factor(cluster)))+geom_point()+
-  geom_point(data=centroids, aes(x=X,y=Y, size=4,shape = as.factor(cluster)))
-
-#FOURTH RUN?
-#Iterate until the cluster assignments stop changing
-#How to detect changes while re-assigning the observations based on distance?
-
-changecounter=0
-for (i in (1:nrow(df))) {
-  # vector to collect the distances for the point to the respective cluster
-  colv=vector()
-  for (j in (1:k)) {
-    mdist=sqrt(((centroids[[j,2]]-df[[i,1]])^2)+((centroids[[j,3]]-df[[i,2]])^2))
-    colv[j]=mdist
-  }
-  newcluster=which.min(colv)
-  if(df[i,'cluster']!=newcluster) {
-    df[i,'cluster']=newcluster
-    changecounter=changecounter+1
-  }
-}
-
-
-
-
-
-
-
-
