e27922d1feb2a3399237f200e418ea658efd16ea
thor <thor@cphbusiness.dk>
2025-02-04 08:45:46 +0100

    pca

--- classAjaxFE.R
+++ classAjaxFE.R
@@ -35,6 +35,7 @@ allAjax=allDutch %>% filter(str_detect(label,"Ajax"))
 idv=allAjax[,'_id']
 # testkampe
 idvt=idv[1:3]
+idvt=idv
 query = jsonlite::toJSON(list(`_id` = list(`$in` = idvt)), auto_unbox = TRUE)
 result=cong$find(query=query, fields = '{}')
 testl=result$events
@@ -50,17 +51,17 @@ av
 avsub=av[1:30]
 avsub2=avsub[-c(3:9)]
 ajaxEventsPassesSub=ajaxEventsPasses[,avsub2]
-ajaxEventsPassesSub$cat=unlist(lapply(allPassesAjax$type.secondary, function(x) x[1]))
+ajaxEventsPassesSub$cat=unlist(lapply(ajaxEventsPassesSub$type.secondary, function(x) x[1]))
 
 
 # find relevant pass-types
 ddf=as.data.frame(table(ajaxEventsPassesSub$cat)) 
-ddf=ddf %>% filter(Freq>30)
+ddf=ddf %>% filter(Freq>410)
 nv=unique(as.character(ddf$Var1))
 
 pstat=ajaxEventsPassesSub %>% group_by(matchId, cat) %>% select(matchId,cat) %>% 
   filter(cat %in% nv) %>% 
-  summarise(value=n(),.groups = "drop")
+  summarise(value=n(),.groups = "drop") %>% 
 ungroup()
 
 
@@ -69,9 +70,41 @@ ajstat=ajaxEventsPassesSub %>% group_by(matchId) %>% mutate(
   passl=mean(pass.length),
   passvar=sd(pass.length),
   totpasses=n(),
-  accratio=round((totpasses - sum(pass.accurate))/totpasses,2),
+  accratio=round((totpasses - sum(pass.accurate))/totpasses,3),
 ) %>% select(accratio,totpasses,matchId,passl,passvar) %>% unique() %>% ungroup()
 colnames(ajstat)
 ajstatLong=ajstat %>% pivot_longer(c(totpasses,passl,passvar,accratio),names_to = "cat",values_to = "value")
 
 totstat=bind_rows(pstat,ajstatLong)
+
+# now wide
+totstat_wide=totstat %>% pivot_wider(names_from = "cat", values_from = "value")
+totstat_wide_scaled=as.data.frame(scale(totstat_wide))
+
+kmod=kmeans(totstat_wide_scaled, nstart = 10, centers =4)
+fviz_cluster(kmod, data = totstat_wide_scaled)
+
+dftwss=data.frame(k=1:20,twss=0)
+for (i in (1:20)) {
+  tmod=kmeans(totstat_wide_scaled,centers = i,nstart = 10)
+  dftwss[i,'twss']=tmod$tot.withinss
+}
+
+plot(dftwss)
+
+kmod=kmeans(totstat_wide_scaled, nstart = 10, centers =3)
+kmod=kmeans(totstat_wide, nstart = 10, centers =3)
+fviz_cluster(kmod, data = totstat_wide)
+  
+# pca
+data.pca <- princomp(totstat_wide_scaled)
+summary(data.pca)
+data.pca$loadings[, 1:2]
+fviz_pca_var(data.pca, col.var = "black")
+
+# hcl
+distm=dist(totstat_wide_scaled)
+hi=hclust(distm,method = "complete")
+plot(hi)
+cut_avg <- cutree(hi, k = 3)
+rect.hclust(hi , k = 3, border = 2:6)
